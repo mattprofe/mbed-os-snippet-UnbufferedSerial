37e9dc1ce6cae624b1d762003fb36a29c68a426f
Matias Baez <mbcorp.matias@gmail.com>
2024-07-13 06:43:15 +0000

    Se simplifico el proyecto

--- main.cpp
+++ main.cpp
@@ -6,24 +6,10 @@
 #include "mbed.h"
 
 // Create a DigitalOutput object to toggle an LED whenever data is received.
-static DigitalOut led(LED1);
+DigitalOut led(LED1);
 
 // Create a UnbufferedSerial object with a default baud rate.
-static UnbufferedSerial serial_port(USBTX, USBRX);
-
-void on_rx_interrupt()
-{
-    char c;
-
-    // Toggle the LED.
-    led = !led;
-
-    // Read the data to clear the receive interrupt.
-    if (serial_port.read(&c, 1)) {
-        // Echo the input back to the terminal.
-        serial_port.write(&c, 1);
-    }
-}
+UnbufferedSerial serial_port(USBTX, USBRX);
 
 int main(void)
 {
@@ -35,6 +21,17 @@ int main(void)
         /* stop bit */ 1
     );
 
-    // Register a callback to process a Rx (receive) interrupt.
-    serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
+    led=1;
+
+    serial_port.write("*", 1);
+
+    char c;
+
+    while(true){
+        if(serial_port.read(&c, 1)){
+            led = !led;
+
+            serial_port.write("#", 1);
+        }
+    }
 }
